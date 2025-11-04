8a4493fb96e38324f8b1102a501b23fa209aee69
RostyslavBoichuk <rostykb2405@gmail.com>
2025-11-04 21:37:35 +0200

    Add files via upload

--- testclass.cs
+++ testclass.cs
@@ -1,224 +1,300 @@
 using System;
+using System.Collections.Generic;
+using MatrixLib;
 using Xunit;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-public class MatrixTests
+namespace MatrixLib.Tests
 {
-    [Fact]
-    public void Constructor_CreatesCorrectDimensions()
+    public class MatrixTests
     {
-        var m = new Matrix(2, 3);
-        Assert.Equal(2, m.Rows);
-        Assert.Equal(3, m.Cols);
+        [Fact]
+        public void Constructor_CreatesCorrectDimensions()
+        {
+            Matrix matrix = new(2, 3);
+            Assert.Equal(2, matrix.Rows);
+            Assert.Equal(3, matrix.Cols);
+        }
+
+        [Fact]
+        public void Constructor_FromArray_CopiesValues()
+        {
+            double[,] arr = { { 1, 2 }, { 3, 4 } };
+            Matrix matrix = new(arr);
+            Assert.Equal(1, matrix[0, 0]);
+            Assert.Equal(4, matrix[1, 1]);
+        }
+
+        [Fact]
+        public void Add_TwoMatrices_ReturnsCorrectSum()
+        {
+            Matrix matrix1 = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Matrix matrix2 = new(new double[,] { { 5, 6 }, { 7, 8 } });
+            Matrix resultMatrix = matrix1.Add(matrix2);
+            Matrix expectedMatrix = new(new double[,] { { 6, 8 }, { 10, 12 } });
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        [Fact]
+        public void Subtract_TwoMatrices_ReturnsCorrectDifference()
+        {
+            Matrix matrix1 = new(new double[,] { { 5, 6 }, { 7, 8 } });
+            Matrix matrix2 = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Matrix resultMatrix = matrix1.Subtract(matrix2);
+            Matrix expectedMatrix = new(new double[,] { { 4, 4 }, { 4, 4 } });
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        [Fact]
+        public void Multiply_ByZeroMatrix_ReturnsZeroMatrix()
+        {
+            Matrix matrix1 = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Matrix zeroMatrix = new(2, 2);
+            Matrix resultMatrix = matrix1.Multiply(zeroMatrix);
+            Assert.Equal(zeroMatrix, resultMatrix);
+        }
+
+        [Fact]
+        public void Multiply_IncompatibleMatrices_Throws()
+        {
+            Matrix matrix1 = new(2, 3);
+            Matrix matrix2 = new(2, 2);
+            Assert.Throws<ArgumentException>(() => matrix1.Multiply(matrix2));
+        }
+
+        [Fact]
+        public void Multiply_IncompatibleMatrices_Throws_WithMessage()
+        {
+            Matrix a = new(2, 3);
+            Matrix b = new(2, 2);
+            var ex = Assert.Throws<ArgumentException>(() => a.Multiply(b));
+            Assert.Contains("incompatible", ex.Message, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void Transpose_SquareMatrix_ReturnsTransposed()
+        {
+            Matrix matrix = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Matrix resultMatrix = matrix.Transpose();
+            Matrix expectedMatrix = new(new double[,] { { 1, 3 }, { 2, 4 } });
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        [Fact]
+        public void Transpose_RectangularMatrix_ReturnsTransposed()
+        {
+            Matrix matrix = new(new double[,] { { 1, 2, 3 }, { 4, 5, 6 } });
+            Matrix resultMatrix = matrix.Transpose();
+            Matrix expectedMatrix = new(new double[,] { { 1, 4 }, { 2, 5 }, { 3, 6 } });
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        [Fact]
+        public void Equals_TwoIdenticalMatrices_ReturnsTrue()
+        {
+            Matrix matrix1 = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Matrix matrix2 = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Assert.True(matrix1.Equals(matrix2));
+        }
+
+        [Fact]
+        public void Equals_TwoDifferentMatrices_ReturnsFalse()
+        {
+            Matrix matrix1 = new(new double[,] { { 1, 2 }, { 3, 4 } });
+            Matrix matrix2 = new(new double[,] { { 4, 3 }, { 2, 1 } });
+            Assert.False(matrix1.Equals(matrix2));
+        }
+
+        [Fact]
+        public void Constructor_ZeroByZero_CreatesEmptyMatrix()
+        {
+            var m = new Matrix(0, 0);
+            Assert.Equal(0, m.Rows);
+            Assert.Equal(0, m.Cols);
+        }
+
+        [Theory]
+        [MemberData(nameof(AdditionData))]
+        public void Add_Theory_TwoMatrices_ReturnsExpected(Matrix matrix1, Matrix matrix2, Matrix expectedMatrix)
+        {
+            Matrix resultMatrix = matrix1.Add(matrix2);
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        public static IEnumerable<object[]> AdditionData()
+        {
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { 1, 1 }, { 1, 1 } }),
+                new Matrix(new double[,] { { 2, 2 }, { 2, 2 } }),
+                new Matrix(new double[,] { { 3, 3 }, { 3, 3 } })
+            };
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { -1, 5 }, { 3, 0 } }),
+                new Matrix(new double[,] { { 1, -2 }, { -3, 4 } }),
+                new Matrix(new double[,] { { 0, 3 }, { 0, 4 } })
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(MultiplicationData))]
+        public void Multiply_Theory_ReturnsExpected(Matrix matrix1, Matrix matrix2, Matrix expectedMatrix)
+        {
+            Matrix resultMatrix = matrix1.Multiply(matrix2);
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        public static IEnumerable<object[]> MultiplicationData()
+        {
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { 1, 2 }, { 3, 4 } }),
+                new Matrix(new double[,] { { 2, 0 }, { 1, 2 } }),
+                new Matrix(new double[,] { { 4, 4 }, { 10, 8 } })
+            };
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { 1, 0 }, { 0, 1 } }),
+                new Matrix(new double[,] { { 9, 8 }, { 7, 6 } }),
+                new Matrix(new double[,] { { 9, 8 }, { 7, 6 } })
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TransposeData))]
+        public void Transpose_Theory_ReturnsExpected(Matrix inputMatrix, Matrix expectedMatrix)
+        {
+            Matrix resultMatrix = inputMatrix.Transpose();
+            Assert.Equal(expectedMatrix, resultMatrix);
+        }
+
+        public static IEnumerable<object[]> TransposeData()
+        {
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { 1, 2, 3 } }),
+                new Matrix(new double[,] { { 1 }, { 2 }, { 3 } })
+            };
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } }),
+                new Matrix(new double[,] { { 1, 3, 5 }, { 2, 4, 6 } })
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(IdentitySizes))]
+        public void Identity_Theory_IsDiagonal(int size)
+        {
+            Matrix identityMatrix = Matrix.Identity(size);
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    Assert.Equal(i == j ? 1 : 0, identityMatrix[i, j]);
+        }
+
+        public static IEnumerable<object[]> IdentitySizes()
+        {
+            yield return new object[] { 0 };
+            yield return new object[] { 1 };
+            yield return new object[] { 2 };
+            yield return new object[] { 5 };
+        }
+
+        [Theory]
+        [MemberData(nameof(AddSubtractReversibilityData))]
+        public void AddThenSubtract_ReturnsOriginal(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix resultMatrix = matrix1.Add(matrix2).Subtract(matrix2);
+            Assert.Equal(matrix1, resultMatrix);
+        }
+
+        public static IEnumerable<object[]> AddSubtractReversibilityData()
+        {
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { 1, 2 }, { 3, 4 } }),
+                new Matrix(new double[,] { { 5, 6 }, { 7, 8 } })
+            };
+            yield return new object[]
+            {
+                new Matrix(new double[,] { { -1, 0 }, { 0, -1 } }),
+                new Matrix(new double[,] { { 2, 3 }, { 4, 5 } })
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(MultiplyByIdentityData))]
+        public void MultiplyByIdentity_ReturnsOriginal(Matrix matrix)
+        {
+            Matrix identityMatrix = Matrix.Identity(matrix.Rows);
+            Matrix resultMatrix = matrix.Multiply(identityMatrix);
+            Assert.Equal(matrix, resultMatrix);
+        }
+
+        public static IEnumerable<object[]> MultiplyByIdentityData()
+        {
+            yield return new object[] { new Matrix(new double[,] { { 1, 2 }, { 3, 4 } }) };
+            yield return new object[] { new Matrix(new double[,] { { 0, 5 }, { -1, 2 } }) };
+            yield return new object[] { new Matrix(new double[,] { { 2 } }) };
+            yield return new object[] { new Matrix(new double[,] { { 1, 2, 3 } }) };
+            yield return new object[] { new Matrix(new double[,] { { 1 }, { 2 }, { 3 } }) };
+        }
+
+        [Theory]
+        [MemberData(nameof(TransposeTwiceData))]
+        public void TransposeTwice_ReturnsOriginal(Matrix matrix)
+        {
+            Matrix resultMatrix = matrix.Transpose().Transpose();
+            Assert.Equal(matrix, resultMatrix);
+        }
+
+        public static IEnumerable<object[]> TransposeTwiceData()
+        {
+            yield return new object[] { new Matrix(new double[,] { { 1, 2 }, { 3, 4 } }) };
+            yield return new object[] { new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 } }) };
+        }
+
+        [Theory]
+        [InlineData(-1, 3)]
+        [InlineData(3, -2)]
+        [InlineData(-4, -4)]
+        public void Constructor_InvalidDimensions_Throws(int rows, int cols)
+        {
+            Assert.Throws<ArgumentException>(() => new Matrix(rows, cols));
+        }
+
+        [Benchmark]
+        public void Multiply_LargeMatrices_Performance()
+        {
+            int n = 200;
+            var a = new Matrix(n, n);
+            var b = new Matrix(n, n);
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = i + j + 1;
+                    b[i, j] = i - j + 1;
+                }
+
+            var result = a.Multiply(b);
+        }
     }
 
-    [Fact]
-    public void Constructor_FromArray_CopiesValues()
+    public class MatrixBenchmarks
     {
-        double[,] data = { { 1, 2 }, { 3, 4 } };
-        var m = new Matrix(data);
-        Assert.Equal(2, m.Rows);
-        Assert.Equal(2, m.Cols);
-        Assert.Equal(3, m[1, 0]);
-    }
+        [Benchmark]
+        public void LargeMatrixMultiplication()
+        {
+            var test = new MatrixTests();
+            test.Multiply_LargeMatrices_Performance();
+        }
 
-    [Fact]
-    public void Add_TwoMatrices_ReturnsCorrectSum()
-    {
-        var a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        var b = new Matrix(new double[,] { { 5, 6 }, { 7, 8 } });
-        var sum = a + b;
-        Assert.Equal(6, sum[0, 0]);
-        Assert.Equal(12, sum[1, 1]);
-    }
-
-    [Fact]
-    public void Subtract_TwoMatrices_ReturnsCorrectDifference()
-    {
-        var a = new Matrix(new double[,] { { 5, 5 }, { 5, 5 } });
-        var b = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        var diff = a - b;
-        Assert.Equal(4, diff[0, 0]);
-        Assert.Equal(1, diff[1, 1]);
-    }
-
-    [Fact]
-    public void Multiply_ByZeroMatrix_ReturnsZeroMatrix()
-    {
-        var a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        var b = new Matrix(2, 2);
-        var result = a * b;
-        Assert.True(result.Equals(new Matrix(2, 2)));
-    }
-
-    [Fact]
-    public void Multiply_IncompatibleMatrices_Throws()
-    {
-        var a = new Matrix(2, 3);
-        var b = new Matrix(4, 2);
-        Assert.Throws<ArgumentException>(() => a * b);
-    }
-
-    [Fact]
-    public void Transpose_SquareMatrix_ReturnsTransposed()
-    {
-        var a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        var t = a.Transpose();
-        Assert.Equal(1, t[0, 0]);
-        Assert.Equal(3, t[0, 1]);
-        Assert.Equal(2, t[1, 0]);
-    }
-
-    [Fact]
-    public void Transpose_RectangularMatrix_ReturnsTransposed()
-    {
-        var a = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 } });
-        var t = a.Transpose();
-        Assert.Equal(3, t.Rows);
-        Assert.Equal(2, t.Cols);
-        Assert.Equal(4, t[1, 0]);
-    }
-
-    [Fact]
-    public void Equals_TwoIdenticalMatrices_ReturnsTrue()
-    {
-        var a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        var b = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        Assert.True(a.Equals(b));
-    }
-
-    [Fact]
-    public void Equals_TwoDifferentMatrices_ReturnsFalse()
-    {
-        var a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
-        var b = new Matrix(new double[,] { { 0, 2 }, { 3, 4 } });
-        Assert.False(a.Equals(b));
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3, 4, 5, 6, 7, 8)]
-    [InlineData(2, 3, 4, 5, 6, 7, 8, 9)]
-    public void Add_Theory_TwoMatrices_ReturnsExpected(double a1, double a2, double a3, double a4, double b1, double b2, double b3, double b4)
-    {
-        var a = new Matrix(new double[,] { { a1, a2 }, { a3, a4 } });
-        var b = new Matrix(new double[,] { { b1, b2 }, { b3, b4 } });
-        var sum = a + b;
-        Assert.Equal(a1 + b1, sum[0, 0]);
-        Assert.Equal(a4 + b4, sum[1, 1]);
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3, 4)]
-    [InlineData(2, 0, 1, 2)]
-    public void Multiply_Theory_ReturnsExpected(double a1, double a2, double a3, double a4)
-    {
-        var a = new Matrix(new double[,] { { a1, a2 }, { a3, a4 } });
-        var id = Matrix.Identity(2);
-        var result = a * id;
-        Assert.Equal(a, result);
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3, 4)]
-    [InlineData(2, 5, 7, 8)]
-    public void Transpose_Theory_ReturnsExpected(double a1, double a2, double a3, double a4)
-    {
-        var a = new Matrix(new double[,] { { a1, a2 }, { a3, a4 } });
-        var t = a.Transpose();
-        Assert.Equal(a1, t[0, 0]);
-        Assert.Equal(a3, t[0, 1]);
-        Assert.Equal(a2, t[1, 0]);
-    }
-
-    [Theory]
-    [InlineData(1)]
-    [InlineData(2)]
-    [InlineData(3)]
-    public void Identity_Theory_IsDiagonal(int size)
-    {
-        var id = Matrix.Identity(size);
-        for (int i = 0; i < size; i++)
-            for (int j = 0; j < size; j++)
-                Assert.Equal(i == j ? 1.0 : 0.0, id[i, j]);
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3, 4)]
-    [InlineData(5, 6, 7, 8)]
-    public void AddThenSubtract_ReturnsOriginal(double a1, double a2, double a3, double a4)
-    {
-        var a = new Matrix(new double[,] { { a1, a2 }, { a3, a4 } });
-        var b = new Matrix(new double[,] { { 1, 1 }, { 1, 1 } });
-        var result = (a + b) - b;
-        Assert.Equal(a, result);
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3, 4)]
-    [InlineData(2, 3, 4, 5)]
-    [InlineData(0, 0, 0, 0)]
-    public void MultiplyByIdentity_ReturnsOriginal(double a1, double a2, double a3, double a4)
-    {
-        var a = new Matrix(new double[,] { { a1, a2 }, { a3, a4 } });
-        var id = Matrix.Identity(2);
-        var result = a * id;
-        Assert.Equal(a, result);
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3, 4)]
-    [InlineData(2, 5, 7, 8)]
-    public void TransposeTwice_ReturnsOriginal(double a1, double a2, double a3, double a4)
-    {
-        var a = new Matrix(new double[,] { { a1, a2 }, { a3, a4 } });
-        var t2 = a.Transpose().Transpose();
-        Assert.Equal(a, t2);
-    }
-
-    [Fact]
-    public void Constructor_NegativeDimensions_Throws()
-    {
-        Assert.Throws<ArgumentException>(() => new Matrix(-1, 2));
-    }
-
-    [Fact]
-    public void Constructor_ZeroDimensions_CreatesEmptyMatrix()
-    {
-        var m = new Matrix(0, 0);
-        Assert.Equal(0, m.Rows);
-        Assert.Equal(0, m.Cols);
-    }
-
-    [Fact]
-    public void Multiply_IncompatibleDimensions_ThrowsWithMessage()
-    {
-        var a = new Matrix(2, 3);
-        var b = new Matrix(2, 2);
-        var ex = Assert.Throws<ArgumentException>(() => a * b);
-        Assert.Contains("incompatible", ex.Message, StringComparison.OrdinalIgnoreCase);
-    }
-
-    [Fact]
-    public void Add_FloatingPointPrecision_TolerantComparison()
-    {
-        var a = new Matrix(new double[,] { { 0.1, 0.2 }, { 0.3, 0.4 } });
-        var b = new Matrix(new double[,] { { 0.2, 0.1 }, { 0.4, 0.3 } });
-        var expected = new Matrix(new double[,] { { 0.3, 0.3 }, { 0.7, 0.7 } });
-        var sum = a + b;
-        for (int i = 0; i < 2; i++)
-            for (int j = 0; j < 2; j++)
-                Assert.True(Math.Abs(sum[i, j] - expected[i, j]) < 1e-9);
-    }
-
-    [Fact]
-    public void Identity_ZeroSize_Throws()
-    {
-        Assert.Throws<ArgumentException>(() => Matrix.Identity(0));
-    }
-
-    [Fact]
-    public void Identity_SingleElement_IsOne()
-    {
-        var id = Matrix.Identity(1);
-        Assert.Equal(1, id[0, 0]);
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MatrixBenchmarks>();
+        }
     }
 }
